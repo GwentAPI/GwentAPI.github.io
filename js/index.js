bcb0fbaeae030ac4b3d64b09cbd317c24cfa550b
Tristan <kraig.tri125@gmail.com>
2016-07-14 23:17:43 -0400

    init

---
+++ js/index.js
@@ -0,0 +1,46 @@
+var host = "http://localhost:8080";
+
+function listResources(){
+	var url = host + "/v0";
+
+	$("#ListResourceExampleLink").attr("href", url);
+	$("#ListResourceExampleLink").text(url);
+
+	$.getJSON(url, function(data, status){
+		var text = JSON.stringify(data, null, 4);
+		$("#ListResourceExample").text(text);
+	});
+}
+
+function retrieveCard(){
+	var url = host + "/v0/cards/triss-mistress-of-magic";
+
+	$("#RetrieveCardLink").attr("href", url);
+	$("#RetrieveCardLink").text(url);
+
+	$.getJSON(url, function(data, status){
+		var text = JSON.stringify(data, null, 4);
+		$("#RetrieveCard").text(text);
+	});
+}
+
+function retrieveCards(){
+	var url = host + "/v0/cards";
+
+	$("#ListCardsLink").attr("href", url);
+	$("#ListCardsLink").text(url);
+
+	$.getJSON(url + "/triss-mistress-of-magic", function(data, status){
+		var text = JSON.stringify(data, null, 4);
+		$("#ListCards").text(text);
+	});
+}
+
+
+
+$(document).ready(function() {
+	listResources();
+	retrieveCard();
+	retrieveCards();
+
+});
