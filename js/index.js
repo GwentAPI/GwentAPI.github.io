d4836f4fa57f056879aec3508fe5dee977978353
Tristan <kraig.tri125@gmail.com>
2017-07-05 21:08:19 -0400

    Update

--- js/index.js
+++ js/index.js
@@ -37,10 +37,22 @@ function retrieveCards(){
 }
 
 
+function scrollFireInit() {
+    var options = [
+      {selector: '#staggered-getStarted', offset: 0, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } }
+    ];
+    Materialize.scrollFire(options);
+}
+
+
 
 $(document).ready(function() {
 	$(".button-collapse").sideNav();
 	listResources();
 	retrieveCard();
 	retrieveCards();
+	scrollFireInit();
+	$('.modal').modal();
 });
