176ff2e3f9ca20cfb54477c18b67e213b0aa4177
Tristan <kraig.tri125@gmail.com>
2017-07-28 00:25:48 -0400

    Removed unused js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-var host = "https://api.gwentapi.com";
-
-function listResources(){
-	var url = host + "/v0";
-
-	$("#ListResourceExampleLink").attr("href", url);
-	$("#ListResourceExampleLink").text(url);
-
-	$.getJSON(url, function(data, status){
-		var text = JSON.stringify(data, null, 4);
-		$("#ListResourceExample").text(text);
-	});
-}
-
-
 function scrollFireInit() {
     var options = [
       {selector: '#staggered-getStarted', offset: -10, callback: function(el) {
@@ -26,9 +11,6 @@ function scrollFireInit() {
 
 $(document).ready(function() {
 	$(".button-collapse").sideNav();
-	listResources();
-	retrieveCard();
-	retrieveCards();
 	scrollFireInit();
 	$('.modal').modal();
 });
